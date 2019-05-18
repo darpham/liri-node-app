39342f647c97ab7f8dfa2321a808f8bc912b9914
Darren Pham <phamdarren1@gmail.com>
2019-05-18 03:44:54 -0700

    optimizations

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
+// Grabbing keys via .env 
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var omdbKey = process.env.OMDB_KEY
+var concertKey = process.env.BIT_KEY
 
 var axios = require('axios');
 
@@ -10,7 +13,7 @@ var apiCalls = {
     apiConcert: function(search) {
         console.log("**calling concert API** search: " + search);
         axios
-        .get('https://rest.bandsintown.com/artists/' + search + '/events?app_id=codingbootcamp')
+        .get('https://rest.bandsintown.com/artists/' + search + '/events?app_id='+concertKey)
         .then(function(response){
             parseData.concert(response, search);
         })
@@ -35,7 +38,7 @@ var apiCalls = {
             console.warn("No input provided, using default");
         }
         axios
-        .get('http://www.omdbapi.com/?apikey=9e91dbeb&t='+search)
+        .get('http://www.omdbapi.com/?apikey='+omdbKey+'&t='+search)
         .then(function(response){
             parseData.movie(response,search);
         })
